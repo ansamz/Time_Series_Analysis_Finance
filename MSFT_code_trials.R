9d67a3d195a6d70fe27475b591cebee1dde71a37
D. Wullschleger <44313663+DaniWullschleger@users.noreply.github.com>
2023-12-09 17:29:32 +0100

    Included Introduction, Removed GARCH parts, Included VAR model and Prediction + Comparison with Testset

--- MSFT_code_trials.R
+++ MSFT_code_trials.R
@@ -1,15 +1,6 @@
-library(PerformanceAnalytics)
-library(forecast)
-library(rugarch)
-library(vars)
-library(tseries)
+# This Script was used to get the Price data on MSFT, so that we can enrich
+# the data with News data and their sentiment scores via python script
+
 library(quantmod)
-library(tinytex)
-library(zoo)
-library(ggpl)
-
 MSFT <- getSymbols.yahoo("MSFT", from="2022-01-01", to="2023-12-01", periodicity = "daily", auto.assign=FALSE)[,6]
-
-head(MSFT)
-
 write.zoo(MSFT, "MSFT.csv", sep = ",", row.names = TRUE)
